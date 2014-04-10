233ff71e5670109d857b124111508f4132606f2e
Daan Reid <osmosisch@gmail.com>
2014-04-10 12:18:10 +0200

    cleanup for standalone app

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,7 +2,6 @@
 define(function(require) {
   var angular = require('angular');
     return angular.module('elicit.controllers', [])
-          .controller('ChooseProblemController', require('controllers/chooseProblem'))
           .controller('WorkspaceController', require('controllers/workspace'))
           .controller('OverviewController', require('controllers/overview'))
           .controller('ScaleRangeController', require('controllers/scaleRange'))
